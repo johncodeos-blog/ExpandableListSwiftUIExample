4295b5e169519ca1e257b038f744b6b2d8e7e7c8
John Codeos <johncodeos@gmail.com>
2020-11-06 14:48:32 +0200

    Initial Commit

---
+++ ExpandableListSwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpandableListSwiftUIExample
+//
+//  Created by John Codeos on 11/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpandableListSwiftUIExample/ExpandableListSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableListSwiftUIExampleApp.swift
+//  ExpandableListSwiftUIExample
+//
+//  Created by John Codeos on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableListSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
